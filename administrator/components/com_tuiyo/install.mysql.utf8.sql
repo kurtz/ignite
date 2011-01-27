e83c03c3343fcc16e397e3bf0ab27741a24d9e07
drstonyhills <livingstonefultang@gmail.com>
2011-01-27 15:15:12 +0000

    Fixes the issues with mysql error 1071 with keys being longer than a 100bytes (varchar < 229)

--- administrator/components/com_tuiyo/install.mysql.utf8.sql
+++ administrator/components/com_tuiyo/install.mysql.utf8.sql
@@ -426,7 +426,7 @@ CREATE TABLE IF NOT EXISTS `#__tuiyo_chat_rooms_users` (
 DROP TABLE IF EXISTS `#__core_acl_acl`;
 CREATE TABLE IF NOT EXISTS `#__core_acl_acl` (
   `id` int(11) NOT NULL DEFAULT '0',
-  `section_value` varchar(230) NOT NULL DEFAULT 'system',
+  `section_value` varchar(220) NOT NULL DEFAULT 'system',
   `allow` int(11) NOT NULL DEFAULT '0',
   `enabled` int(11) NOT NULL DEFAULT '0',
   `return_value` text,
@@ -441,9 +441,9 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_acl` (
 DROP TABLE IF EXISTS `#__core_acl_acl_sections`;
 CREATE TABLE IF NOT EXISTS `#__core_acl_acl_sections` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `value` varchar(230) NOT NULL,
+  `value` varchar(220) NOT NULL,
   `order_value` int(11) NOT NULL DEFAULT '0',
-  `name` varchar(230) NOT NULL,
+  `name` varchar(220) NOT NULL,
   `hidden` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `value_acl_sections` (`value`),
@@ -453,8 +453,8 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_acl_sections` (
 DROP TABLE IF EXISTS `#__core_acl_aco`;
 CREATE TABLE IF NOT EXISTS `#__core_acl_aco` (
   `id` int(11) NOT NULL DEFAULT '0',
-  `section_value` varchar(240) NOT NULL DEFAULT '0',
-  `value` varchar(240) NOT NULL,
+  `section_value` varchar(220) NOT NULL DEFAULT '0',
+  `value` varchar(220) NOT NULL,
   `order_value` int(11) NOT NULL DEFAULT '0',
   `name` varchar(255) NOT NULL,
   `hidden` int(11) NOT NULL DEFAULT '0',
@@ -466,15 +466,15 @@ CREATE TABLE IF NOT EXISTS `#__core_acl_aco` (
 DROP TABLE IF EXISTS `#__core_acl_aco_map`;
 CREATE TABLE IF NOT EXISTS `#__core_acl_aco_map` (
   `acl_id` int(11) NOT NULL DEFAULT '0',
-  `section_value` varchar(230) NOT NULL DEFAULT '0',
-  `value` varchar(230) NOT NULL,
+  `section_value` varchar(220) NOT NULL DEFAULT '0',
+  `value` varchar(220) NOT NULL,
   PRIMARY KEY (`acl_id`,`section_value`,`value`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT="added by com_tuiyo";
 
 DROP TABLE IF EXISTS `#__core_acl_aco_sections`;
 CREATE TABLE IF NOT EXISTS `#__core_acl_aco_sections` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `value` varchar(230) NOT NULL,
+  `value` varchar(220) NOT NULL,
   `order_value` int(11) NOT NULL DEFAULT '0',
   `name` varchar(230) NOT NULL,
   `hidden` int(11) NOT NULL DEFAULT '0',
